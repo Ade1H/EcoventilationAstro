a2983458b05a9c8a26ac798f23feb05523d9830c
Ade1H <adelhosseini1986@gmail.com>
2026-02-17 16:39:08 +0100

    fixa map iconen

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-const Map = ({ lat = 60.674, lng = 17.141, zoom = 13, markerText = "Ecoventilation - Gävle" }) => {
+// Fix default icons using Leaflet CDN
+if (typeof window !== "undefined") {
+  delete L.Icon.Default.prototype._getIconUrl;
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl:
+      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  });
+}
+
+const Map = ({
+  lat = 60.674,
+  lng = 17.141,
+  zoom = 13,
+  markerText = "Ecoventilation - Gävle",
+}) => {
   return (
     <div className="w-full h-80 md:h-96 rounded-lg overflow-hidden shadow-lg">
-      <MapContainer center={[lat, lng]} zoom={zoom} scrollWheelZoom={false} className="w-full h-full">
+      <MapContainer
+        center={[lat, lng]}
+        zoom={zoom}
+        scrollWheelZoom={false}
+        className="w-full h-full"
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
